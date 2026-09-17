1cc0fc4e12538aca3bf07fd97263fd39d3d4a53a
aristidenkoa17@gmail.com <aristidenkoa17@gmail.com>
2026-09-17 15:14:36 +0100

    Fichier 1 modifier

--- Fichier_1.cpp
+++ Fichier_1.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main(){
+    int n = 3;
     std::cout << "Hello, World!" << std::endl;
+
+    for(int i = 1; i <= n; i++){
+        std::cout << "Comment allez-vous ?" << std::endl;
+    }
+    
     return 0;   
 }
