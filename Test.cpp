d7326a84e1ceda050b21516dda60889f2e10c195
aristidenkoa17@gmail.com <aristidenkoa17@gmail.com>
2026-09-19 17:38:51 +0100

    Premier commit

---
+++ Test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main (){
+    std::string name = "Alice";
+    int age = 30;
+
+    std::cout << "Nom: " << name << ", Âge: " << age << std::endl;
+
+    return 0;
+}
