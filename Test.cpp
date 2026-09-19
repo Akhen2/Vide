131629e85bcd1c1dfef75945fe4e1dfb90051048
aristidenkoa17@gmail.com <aristidenkoa17@gmail.com>
2026-09-19 17:41:58 +0100

    Modification du fichier Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -4,8 +4,9 @@
 int main (){
     std::string name = "Alice";
     int age = 30;
+    int annee = 2007;
 
-    std::cout << "Nom: " << name << ", Âge: " << age << std::endl;
+    std::cout << "Nom: " << name << ", Âge: " << age << ", Je suis né en :" << annee << std::endl;
 
     return 0;
 }
