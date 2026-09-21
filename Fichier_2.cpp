ed33184c882e437aee5918b47313019fc0fe0990
aristidenkoa17@gmail.com <aristidenkoa17@gmail.com>
2026-09-21 18:53:02 +0100

    Modification de l'exercice 4

--- Fichier_2.cpp
+++ Fichier_2.cpp
@@ -3,10 +3,10 @@
 int main(){
     int a = 3;
     int b = 2;
-    float c = 4.5;
-    float sum = a + b + c;
+    float c = 4.6;
+    float sum = a + b * c;
 
-    std::cout << "La somme de " << a << ", " << b << " et " << c << " est: " << sum << std::endl;
+    std::cout << "Le calcul de " << a << ", " << b << " et " << c << " donne: " << sum << std::endl;
 
     return 0;
 }
